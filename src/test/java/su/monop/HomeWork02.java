600bbd93fbf24e0f13cebb51b2454a3beca4b494
saynomeow <aleksey.golikov11@gmail.com>
2021-12-22 00:31:43 +0300

    file for testing dowload pic feature

--- src/test/java/su/monop/HomeWork02.java
+++ src/test/java/su/monop/HomeWork02.java
@@ -1,9 +1,13 @@
 package su.monop;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class HomeWork02PracticeForm {
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class HomeWork02 {
     //arrange
     @BeforeAll
     static void beforeAll() {
@@ -13,6 +17,30 @@ public class HomeWork02PracticeForm {
     @Test
     void fillPracticeFormTests() {
         open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Lex");
+        $("#lastName").setValue("G");
+        $("#userEmail").setValue("123@test.com");
+        $("[for='gender-radio-3']").click();
+        $("#userNumber").setValue("9991234567");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("August");
+        $(".react-datepicker__year-select").selectOption("1986");
+        $(".react-datepicker__day--012").click();
+
+        $("#subjectsInput").setValue("English").pressEnter();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $("[for=hobbies-checkbox-1]").click();
+        $("[for=hobbies-checkbox-3]").click();
+
+        $("#uploadPicture").uploadFromClasspath("");
+
+        $("#currentAddress").setValue("My current address");
+        $("#react-select-3-input").setValue("Rajasthan").pressEnter();
+        $("#react-select-4-input").setValue("Jaipur").pressEnter();
+
+        $("#submit").click();
+
 
 
     }
