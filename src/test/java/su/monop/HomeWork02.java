c7be790baffcbe636f50fe4502325520693ea497
saynomeow <aleksey.golikov11@gmail.com>
2021-12-22 16:52:28 +0300

    upd by note

--- src/test/java/su/monop/HomeWork02.java
+++ src/test/java/su/monop/HomeWork02.java
@@ -15,43 +15,38 @@ public class HomeWork02 {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
+    Configuration.baseUrl = "https://demoqa.com";
     }
 
    //act
     @Test
     void testFillThePracticeForm() {
-        open("https://demoqa.com/automation-practice-form");
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         $("#firstName").setValue("Lex");
         $("#lastName").setValue("G");
         $("#userEmail").setValue("123@test.com");
         $("[for='gender-radio-3']").click();
-
         $("#userNumber").setValue("9991118877");
-
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("August");
-        $(".react-datepicker__year-select").selectOption("1986");
-        $(".react-datepicker__day--012").click();
-
+        $(".react-datepicker__month-select").selectOption("October");
+        $(".react-datepicker__year-select").selectOption("1993");
+        $(".react-datepicker__day--010:not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").setValue("Chemistry").pressEnter();
         $("#subjectsInput").setValue("Social Studies").pressEnter();
-
         $("[for=hobbies-checkbox-2]").click();
         $("[for=hobbies-checkbox-3]").click();
-
         $("#uploadPicture").uploadFromClasspath("new.txt");
-
         $("#currentAddress").setValue("My current address 123");
-
         $("#react-select-3-input").setValue("Rajasthan").pressEnter();
         $("#react-select-4-input").setValue("Jaipur").pressEnter();
-
         $("#submit").click();
 
        //assert
         $("#example-modal-sizes-title-lg").shouldBe(visible);
-        $(".table-responsive").shouldHave(text("Lex"), text("G"), text("123@test.com"),
-          text("Other"), text("9991118877"), text("12 August,1986"), text("Chemistry, Social Studies"),
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Lex G"), text("123@test.com"),
+          text("Other"), text("9991118877"), text("10 October,1993"), text("Chemistry, Social Studies"),
           text("Reading, Music"), text("new.txt"), text("My current address 123"), text("Rajasthan Jaipur"));
     }
 }
